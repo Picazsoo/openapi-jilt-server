cbfcb12771d39b4b34f5ff64dfb98f5d81f0d7ee
Metlička Jáchym <metlicka@2n.com>
2023-04-24 02:13:07 +0200

    2D_take ownership of UsersApiController. Copy it to regular sources and stop regenerating of it via .openapi-generator-ignore

---
+++ src/main/java/cz/metlicka/server/controller/UsersApiController.java
@@ -0,0 +1,28 @@
+package cz.metlicka.server.controller;
+
+import cz.metlicka.server.api.UsersApi;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+
+@Controller
+@RequestMapping("${openapi.userServiceContract.base-path:}")
+public class UsersApiController implements UsersApi {
+
+    private final NativeWebRequest request;
+
+    @Autowired
+    public UsersApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
